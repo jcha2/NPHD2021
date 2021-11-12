9822f64c7b3f01c355c7cc71f657218d07a32df0
Gistar <progistar@naver.com>
2021-11-13 08:19:32 +0900

    refactor: added comments and somewhat rearrangment of piece of codes
    I realized that there were lots of things to do...
    1) Taking arguments for hyper-parameters, input path
       It will be helpful to find better parameters automatically
    2) Should introduce Cross validation (CV) strategy
    3) Checking random_split function
    4) The current CoAtNet was very-very slow when change input vector size as 512x512
       Should change the code for GPU version
    5) Making a function to evaluate performance such as: a) accuracy, b) specificity, c) sensitivity, d) precision, e) negative predicable value, and f) F1 score, later!
    
    What a wonderful project...

--- src/main.py
+++ src/main.py
@@ -54,59 +54,73 @@ class histo_cancer_dataset(Dataset):
 	
 if __name__ == '__main__':
 	# load tensorboard for pytorch
-	writer = SummaryWriter()
+	# Unimplemented yet
+	# writer = SummaryWriter()
+
+	# Set basic things
+	## Set seed for reproducibility
 	torch.manual_seed(1004)
 	print(torch.__version__)
 
-	histo_dataset = histo_cancer_dataset("./kTest/")
+	# Load dataset
+	## It must be modified to take arguments from a user
+	histo_dataset = histo_cancer_dataset("/home/bis/211105_CSH_MPL/CoAtNet/kTest/")
 	print(len(histo_dataset))
 	
+	# Divide train and validation dataset
+	## TODO: 4, 5, 8, 10 fold CV should be considered in the future
+	## Currently, just split into 8:2
 	dSize = len(histo_dataset)
 	nTrains = int(0.8*dSize)
 	nVals = dSize - nTrains
-
+	
+	## TODO: Check if random_split preserves 1:1 ratio of positives and negatives
 	trains, vals = random_split(histo_dataset, [nTrains, nVals])	
 
+	# Convert images into tensors
 	trains = DataLoader(trains, batch_size=32, shuffle=True)
 	vals = DataLoader(vals, batch_size=32, shuffle=False)
-
+	
+	# Check size
 	print(len(trains))
 	print(len(vals))
-	
-	for x, y in trains:
-		print(x.shape)
-		print(y.shape)
-		break
 
+	# Hyperparameters for CoAtNet-7
+	## TODO: Which one is the best?
 	num_blocks = [2, 2, 3, 5, 2]
 	channels = [64, 96, 192, 384, 768]
 	block_types = ['C', 'T', 'T', 'T']
-
+	
+	## Load CoAtNet
 	net = CoAtNet((224, 224), 3, num_blocks, channels, num_classes = 2, block_types = block_types)
 
-	criterion = torch.nn.Softmax(dim=1)
-	optimizer = torch.optim.Adam(net.parameters(), lr = 0.0000001)
-	loss = torch.nn.CrossEntropyLoss()
+	# Loss function
+	## CrossEntropyLoss is a single function combining LogSoftMax and NLLLoss (Log Likelihood Loss)
+	loss = torch.nn.CrossEntropyLoss()	
 
-	iter_=0
+	# Update strategy
+	optimizer = torch.optim.Adam(net.parameters(), lr = 0.0000001)
+
+	# Notation
+	## epoch: the number of times an algorithm visits all dataset
+	## iteration: the number of times an algorithm vists a batch
 	epoch_=5
 	for ep in range(epoch_):
+		iter_ = 0
 		print(f'epoch: {ep}')
 		for x, y in trains:
-			out = net(x)
-			y_pred = criterion(out)
-
+			y_pred = net(x)
+			
 			_, y_pred_tags = torch.max(y_pred, dim = 1)
 			correct_pred = (y_pred_tags == y).float()
 			acc = correct_pred.sum() / len(correct_pred)
 
 			print(f'{acc:.3f}')
-			loss_ = loss(out, y)
+			loss_ = loss(y_pred, y)
 			print(loss_)
 			loss_.backward()
 			optimizer.step()
 			optimizer.zero_grad()
 			iter_ = iter_ + 1
-			print(iter_)
-#	print(out.shape, coatnet.count_parameters(net))
+			print(f'iter: {iter_}')
 
